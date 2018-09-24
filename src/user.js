7e4a99ebbecbd10e56e0bbd116fae1573d2bf022
songjunjie <songjunjie@advanpro.hk>
2018-09-24 09:40:11 +0800

    uu

---
+++ src/user.js
@@ -0,0 +1,52 @@
+export default {
+  eagle: {
+    label: '夜鹰',
+    mark: '拔剑最快 & 职业电灯泡',
+    uid: 'eagle'
+  },
+  eleven: {
+    label: '十一',
+    mark: '神勇爆头男',
+    uid: 'eleven'
+  },
+  dove: {
+    label: '白鸽',
+    mark: '双节棍公主',
+    uid: 'dove'
+  },
+  fire: {
+    label: '黑火',
+    mark: '托尼贾',
+    uid: 'fire'
+  },
+  sky: {
+    label: '天啸',
+    mark: '从事辞职事业多年',
+    uid: 'sky'
+  },
+  falcon: {
+    label: '夜隼',
+    mark: '大魔王 & 夜鹰小徒弟',
+    uid: 'falcon'
+  },
+  swallow: {
+    label: '夜燕',
+    mark: '除了张敬轩，我还爱C罗',
+    uid: 'swallow'
+  },
+  leopard: {
+    label: '青豹',
+    mark: '有空一起做俯卧撑啊',
+    uid: 'leopard'
+  },
+  master: {
+    label: '黄药师',
+    mark: '大家吃好喝好，喝好吃好',
+    uid: 'master'
+  },
+  leaf: {
+    label: '木叶',
+    mark: '吉祥物',
+    uid: 'leaf'
+  }
+}
